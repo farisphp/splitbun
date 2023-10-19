f3f604a8ff719adb0de6e58e6ec3170fc9dfcd4f
Faris Perwira <faris.perwira@gmail.com>
2023-10-19 23:57:12 +0800

    update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  target: "es2015",
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  format: ["esm", "cjs", "iife"],
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
   clean: true,
+  minify: true,
 });
